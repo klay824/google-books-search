42e9e3414a9aa4dc5f6cad50dea12a77b5d64a14
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 18:47:19 -0500

    removed unnecessary link import

--- client/src/components/SavedCard/index.js
+++ client/src/components/SavedCard/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "react-router-dom/Link";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
