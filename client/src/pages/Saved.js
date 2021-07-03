ea5b27afe9bededaefdf6a73998cf8e258c34ba2
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 18:32:50 -0500

    got saved books loading to saved page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
-import Heading from "../components/Heading.js";
-import SavedCard from "../components/SavedCard.js";
+import Heading from "../components/Heading";
+import SavedCard from "../components/SavedCard";
 
 function Saved() {
+    // setting initial state
+    const [books, setBooks] = useState([])
+
+    // loads all books and stores them with setBooks
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    function loadBooks() {
+        API.getBooks()
+            .then(res =>
+                setBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
+    // deletes books
+    function deleteBook(id) {
+        API.deleteBook(id)
+            .then(res => loadBooks())
+            .catch(err => console.log(err));
+    };
+
     return (
         <>
             <Heading />
-            <SavedCard />
+            {books.map(book => (
+                <SavedCard key={book._id} title={book.title} author={book.author} description={book.description} image={book.image} link={book.link}></SavedCard>
+            ))}
+
         </>
-    )
+    );
 }
 
 export default Saved;
