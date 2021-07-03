364c2604bacb3e0cd4c3427b0e9ed74fe9069c78
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 16:14:52 -0500

    wrote resultscard, still wip

---
+++ client/src/components/ResultsCard.js/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+
+function ResultsCard() {
+    return (
+        <Card variant="outlined">
+            <CardContent>
+                <h5>Results</h5>
+            </CardContent>
+            <CardContent>
+                <Card variant="outlined">
+                    <CardContent>Results will go in here
+                        <CardActions>
+                            <Button size="small">View</Button>
+                            <Button size="small">Save</Button>
+                        </CardActions>
+                    </CardContent>
+                </Card>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default ResultsCard;
