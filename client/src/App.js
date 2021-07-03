0abd30f35e869d55d6a994ab93ca286549d2debc
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 14:52:43 -0500

    imported Search and Saved pages, set up Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Nav from "./components/Nav";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 
 function App() {
   return (
-    <div className="App">
-      <p>Greetings, from React!</p>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/saved" component={Saved} />
+        <Route exact path="/search" component={Search} />
+      </div>
+    </Router>
   );
 }
 
