2fba4c11821cffe5d9aad70276e8cd88dab1dcf7
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 13:53:22 -0500

    wrote the api calls to Google Books and the backend

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    // call to the Google Books api
+    callGoogle: function () {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    },
+    // Gets all saved books from database
+    getBooks: function () {
+        return axios.get("/api/books");
+    },
+    // deletes saved book from database
+    deleteBook: function () {
+        return axios.delete("/api/books/" + id);
+    },
+    // saves new book to database
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData)
+    }
+};
