9cde9b89742a5524050cc72426912b120c4bed48
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 10:45:22 -0500

    renamed books.js to book.js in models

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: String,
+    author: String,
+    description: String,
+    image: String,
+    link: String
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
