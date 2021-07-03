204b9373401d793e48dfc333f8f38cd504512f56
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 18:34:05 -0500

    added parameters to callGoogle and deleteBook functions

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     // call to the Google Books api
-    callGoogle: function () {
+    callGoogle: function (query) {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
     },
     // Gets all saved books from database
@@ -10,7 +10,7 @@ export default {
         return axios.get("/api/books");
     },
     // deletes saved book from database
-    deleteBook: function () {
+    deleteBook: function (id) {
         return axios.delete("/api/books/" + id);
     },
     // saves new book to database
