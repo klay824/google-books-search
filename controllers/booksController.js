3a6abfe8eba94af19193dfaa9508be48dd83eeff
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 10:46:50 -0500

    rewrote schema based on Mongoose schema documentation (URL and Image are not proper schema types)

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -5,13 +5,7 @@ module.exports = {
     findAll: function (req, res) {
         db.Book
             .find(req.query)
-            .sort({ date: -1 })
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    findById: function (req, res) {
-        db.Book
-            .findById(req.params.id)
+            .sort({ _id: -1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
