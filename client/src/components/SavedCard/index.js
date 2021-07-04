07086182fdf6f553f5de5e1c4a1b4b85aa05f433
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 20:04:00 -0500

    removed old import for cardactionarea

--- client/src/components/SavedCard/index.js
+++ client/src/components/SavedCard/index.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 
