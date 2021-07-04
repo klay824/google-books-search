ad18719ca8c68cdf3d50363226170c1f383a5701
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-03 22:01:30 -0500

    got data console logging the default search Star Wars

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import Heading from "../components/Heading";
 import SearchForm from "../components/Form";
 import ResultsCard from "../components/ResultsCard";
+import API from "../utils/API";
 
-function Search() {
-    return (
-        <>
-            <Heading />
-            <SearchForm />
-            <ResultsCard />
-        </>
-    )
+class Search extends Component {
+    state = {
+        search: "",
+        results: []
+    };
+
+    // when the component loads, we will search for Star Wars books
+    componentDidMount() {
+        this.searchGoogleBooks("star wars");
+    }
+
+    searchGoogleBooks = query => {
+        API.callGoogle(query)
+            .then(res => {
+                console.log(res);
+                this.setState({ results: res.data.items })
+            })
+            .catch(err => console.log(err));
+    };
+
+    handleInputChange = event => {
+        const name = event.target.name;
+        const value = event.target.name;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.searchGoogleBooks(this.state.search);
+    };
+
+    render() {
+        return (
+            <>
+                <Heading />
+                <SearchForm
+                    search={this.state.search}
+                    handleFormSubmit={this.handleFormSubmit}
+                    handleInputChange={this.handleInputChange}
+                />
+                <ResultsCard results={this.state.results} />
+            </>
+        )
+    }
 }
 
 export default Search;
