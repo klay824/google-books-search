949d961f9ff57bd1de2d613764eff404f67c41dc
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-04 10:33:41 -0500

    copied in SavedCard and altered for ResultsCard

--- client/src/components/ResultsCard/index.js
+++ client/src/components/ResultsCard/index.js
@@ -1,27 +1,50 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 
-function ResultsCard(props) {
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 900,
+    },
+    media: {
+        height: 192,
+        width: 128,
+        flex: "left",
+    },
+});
+
+function Search({ title, author, description, image, link }) {
+    const classes = useStyles();
     return (
         <Card variant="outlined">
             <CardContent>
-                <h5>Results</h5>
+                <h5>Saved Books</h5>
             </CardContent>
             <CardContent>
                 <Card variant="outlined">
-                    <CardContent>Results will go in here
-                        <CardActions>
-                            <Button size="small">View</Button>
-                            <Button size="small">Save</Button>
-                        </CardActions>
-                    </CardContent>
+                    <CardContent>{title}</CardContent>
+                    <CardContent>Author: {author}</CardContent>
+                    <CardContent>Description: {description}</CardContent>
+
+                    <CardMedia
+                        className={classes.media}
+                        image={image}
+                        title={title}
+                    />
+
+                    <CardActions>
+                        <Button size="small" href={link} target="_blank">View</Button>
+                        <Button size="small" >Save</Button>
+                    </CardActions>
+
                 </Card>
             </CardContent>
-        </Card>
+        </Card >
     )
 }
 
-export default ResultsCard;
+export default Search;
