d9be40492850b0afb9caadf693c6f4c8ab8a7747
katelyn chadwell <kchadwell0226@gmail.com>
2021-07-06 15:03:40 -0500

    ran npm run-scripts build

---
+++ client/build/precache-manifest.a87f56916c7a84e0521cec4c103667ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35b090c913bf16c11c4afafafc6d401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1efce07b8382c266c383",
+    "url": "/static/js/2.d4f9ea89.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.d4f9ea89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0664630fc80baeabd7",
+    "url": "/static/js/main.3d72496d.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
